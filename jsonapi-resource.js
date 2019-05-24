a930c9e7d5a90b17784e42be8c0165cdeaf14694
Weengs <weengs@moskitohero.com>
2019-05-24 03:58:02 +0200

    Improvements

--- jsonapi-resource.js
+++ jsonapi-resource.js
@@ -5,6 +5,10 @@ import 'axios'
 import Jsona from 'jsona'
 
 class JsonapiResource extends LitElement {
+
+  /*
+  ** Constructor
+  */
   constructor () {
     super()
     this.axios = require('axios')
@@ -12,65 +16,91 @@ class JsonapiResource extends LitElement {
     this.url = ''
     this.resource = ''
     this.response = {}
-    this.setup()
   }
+
+  /*
+  ** Properties
+  */
   static get properties () {
     return {
+      /*
+      ** The base URL of the API, no trailing slash
+      */
       url: {
         type: String
       },
+      /*
+      ** The resource to call
+      ** eg. /authors/17.json
+      */
       resource: {
         type: String
       },
+      /*
+      ** The response given by the server
+      */
       response: {
         type: Object
       },
+      /*
+      ** The jsonapi-formatted data
+      */
       data: {
         type: Object
       },
-      normalized: {
+      /*
+      ** The deserialized data
+      */
+      deserialized: {
         type: Object
       },
+      /*
+      ** The computed path, composed of url and resource
+      */
       path: {
         type: String
       }
     }
   }
-  /**
-   *
-   */
-  render () {
-    return html`
-      <!-- template content -->
-      <p>${this.path}</p>
-    `
-  }
 
+  /*
+  ** Called when the element is updated
+  */
   updated (props) {
     if (props.has('response')) { // url has changed
       this.data = this.response.data
+      if (!!this.data) {
+        this.deserialized = this.formatter.deserialize(this.data)
+      }
+      let event = new CustomEvent('response-changed', { detail: { ok: !!this.response.data } })
+      this.dispatchEvent(event)
     }
     if (props.has('url')) {
       this.get()
     }
+    if (props.has('deserialized')) {
+      this.data = this.formatter.serialize({stuff: this.deserialized})
+    }
   }
 
+  /*
+  ** This is the function to call when deserialized data is to be updated
+  */
+  updateData (data) {
+    this.deserialized = data
+    this.data = this.formatter.serialize({stuff: data})
+  }
+
+  /*
+  ** path getter
+  */
   get path () {
     return this.url + this.resource
   }
 
-  get deserialized () {
-    return this.formatter.deserialize(this.response.data)
-  }
-
-  set deserialized (value) {
-    this.deserialized = value
-    this.data = this.formatter.serialize(value)
-  }
-
-  setup () {
-  }
-
+  /*
+  ** API DELETE request
+  */
   delete () {
     const that = this
     this.axios.get(this.path)
@@ -85,6 +115,9 @@ class JsonapiResource extends LitElement {
       )
   }
 
+  /*
+  ** API GET request
+  */
   get () {
     const that = this
     this.axios.get(this.path)
@@ -99,6 +132,9 @@ class JsonapiResource extends LitElement {
       )
   }
 
+  /*
+  ** API POST request
+  */
   post () {
     const that = this
     this.axios.post(this.path, this.data)
@@ -113,6 +149,9 @@ class JsonapiResource extends LitElement {
       )
   }
 
+  /*
+  ** API PATCH request
+  */
   patch () {
     const that = this
     this.axios.patch(this.path, this.data)
@@ -127,18 +166,30 @@ class JsonapiResource extends LitElement {
       )
   }
 
+  /*
+  ** API PUT request
+  */
   put () {
     this.patch()
   }
 
+  /*
+  ** Successful response callback
+  */
   handleResponse (response) {
     this.response = response
   }
 
+  /*
+  ** Error response callback
+  */
   handleError (error) {
     this.error = error
   }
 
+  /*
+  ** Post response callback
+  */
   afterRequest () {
   }
 }
